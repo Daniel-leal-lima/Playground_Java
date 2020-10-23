032f81fd474554c8ea92b63bed878b5f33618f78
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2020-10-23 08:07:07 -0300

    game loop profissional

--- Game.java
+++ Game.java
@@ -1,12 +1,24 @@
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
     public static boolean isRunning = false;
     public Thread thread;
+    public JFrame jFrame;
     public static final int WIDTH = 160;
     public static final int HEIGHT = 120;
     public static final int SCALE = 4;
 
+    public Game(){
+        this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+        jFrame = new JFrame("PRIMEIRO JOGO FEITO EM JAVA");
+        jFrame.add(this);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
@@ -19,17 +31,36 @@ public class Game extends Canvas implements Runnable{
     }
     
     private void tick() {
-        System.out.println("tick");
+        //System.out.println("tick");
     }
 
     private void render() {
-        System.out.println("Render");
+        //System.out.println("Render");
     }
     @Override
     public void run() {
+        long tempo = System.nanoTime();
+        double tick = 60.0;
+        double ns = 1000000000/tick;
+        double delta =0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
         while (isRunning){
-            tick();
-            render();
+            long now = System.nanoTime();
+            delta+= (now-tempo)/ns;
+            tempo=now;
+            if (delta>=1){
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+            if (System.currentTimeMillis() - timer >= 1000){
+                System.out.println(frames);
+                frames = 0;
+                timer +=1000;
+            }
+            
         }
     }
 
